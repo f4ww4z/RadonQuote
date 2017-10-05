3bfab31a87d696e442296730630a42283a621dc8
zentech <jlvazquez825@gmail.com>
2017-10-05 09:19:47 -0400

    second commit

--- app/src/main/java/com/example/george/radonquote/MainActivity.java
+++ app/src/main/java/com/example/george/radonquote/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.george.radonquote;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar myToolbar;
+    String randQuote;
+    TextView quoteTextView;
+    ImageView bgImageView;
+    String url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting the ActionBar for the activity
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+    }
+
+    /*
+    * async class to get random quote in background
+    */
+    class RandomQuote extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this,"Fetching Quote!",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "http://api.androidhive.info/contacts/";
+            String jsonStr = sh.makeServiceCall(url);
+            return null;
+        }
     }
 }
